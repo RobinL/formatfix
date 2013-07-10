187940e978c8ec726e0dd782162183ddb1902bbe
Robin Linacre <robinlinacre@hotmail.com>
2013-07-10 10:16:27 +0100

    formats data loaded

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 $(function() {
-	$("#myp").html("The script works");
-	$('#myp').load('ajax/formats.txt', function() {
-		$('#myp').append("..and the callback fired like a boss");
+
+	$.get('ajax/formats.txt', function(data) {
+		
+		
 	});
+
+
 });
